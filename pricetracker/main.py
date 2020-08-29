08eef9ee75c8ac616e2017b1fb27de314bf25943
Hongze Xia <hongze.x@gmail.com>
2020-08-30 07:59:20 +1000

    WIP: background task

--- pricetracker/main.py
+++ pricetracker/main.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
@@ -5,7 +7,8 @@ from .api.page import router as page_router
 from .api.price import router as price_router
 from .api.user import router as user_router
 from .api.website_config import router as config_router
-from .config import config
+from .config import config, logger
+from .task import check_db
 
 app = FastAPI(
     debug=config.debug,
@@ -24,3 +27,11 @@ app.include_router(user_router, prefix='/user',  tags=['User'])
 app.include_router(page_router, prefix='/page',  tags=['Page'])
 app.include_router(price_router, prefix='/price',  tags=['Price'])
 app.include_router(config_router, prefix='/website-config',  tags=['WebsiteConfig'])
+
+
+@app.on_event("startup")
+def on_startup():
+    logger.info("starting background tasks...")
+    t = Thread(target=check_db, daemon=True)
+    t.start()
+    logger.info("done...")
---
+++ pricetracker/task.py
@@ -0,0 +1,66 @@
+import time
+from datetime import datetime, timedelta
+from typing import List, Optional, Tuple, Union
+
+from .config import config, logger
+from .models import Page, Price, WebsiteConfig, create_session_auto
+from .pushover import send_message
+from .webdriver import track
+
+
+def get_outdated_pages_and_configs() -> List[Tuple[Page, WebsiteConfig]]:
+    with create_session_auto() as sess:
+        return (
+            sess
+            .query(Page, WebsiteConfig)
+            .filter(Page.updated_time + timedelta(hours=Page.freq) < datetime.now())
+            .join(WebsiteConfig)
+            .all()
+        )
+
+
+def get_prices(page: Page, limit=30, last_only=False) -> Union[Optional[Price], List[Price]]:
+    with create_session_auto() as sess:
+        q = (
+            sess.query(Price)
+            .filter(Price.page_id == page.id)
+            .order_by(Price.created_time.desc())
+        )
+        if last_only:
+            return q.first()
+        return q.limit(limit)
+
+
+def add_price(page: Page, price: str):
+    with create_session_auto() as sess:
+        price_orm = Price(price=price, page_id=page.id)
+        sess.add(price_orm)
+
+
+def compose_message(page: Page, conf: WebsiteConfig, current_price: str, last_price: str, prices: List[Price]):
+    return f"""{page.name} price changed from {last_price} to {current_price}\n"""
+
+
+def check_db():
+    while True:
+        logger.info('checking db...')
+        try:
+            pages_configs = get_outdated_pages_and_configs()
+            if not pages_configs:
+                logger.info("no outdated pages found")
+                continue
+            for page, conf in pages_configs:
+                current_price = track(page.url, conf.xpath)
+                last_price = get_prices(page, last_only=True)
+                add_price(page, current_price)
+                if last_price is None:
+                    logger.info(f"found the first price for {page.name}")
+                    continue
+                if current_price == last_price.price:
+                    logger.info(f"found the same price for {page.name}")
+                    continue
+                prices = get_prices(page, limit=config.fetch_limit)
+                send_message(compose_message(page, conf, current_price, last_price.price, prices))
+        except:
+            logger.exception("encountered exception")
+        time.sleep(config.check_freq)
